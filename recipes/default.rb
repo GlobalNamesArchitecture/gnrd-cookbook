35aee9fe5fc2894c9bacb755d75e38a536bb19b6
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-05-21 13:12:31 -0400

    templates added

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,50 @@
+apache_module 'proxy'
+
+apache_module 'proxy_balancer'
+
+apache_module 'proxy_http'
+
+apache_module 'rewrite'
+
+directory "/etc/thin" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+template '/vagrant/config.yml' do
+  source 'db_config.erb'
+  mode '0440'
+  owner 'root'
+  group 'root'
+end
+
+template '/etc/apache2/sites-available/default' do
+  source 'apache_default_site.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
+link '/etc/apache2/sites-enabled/000-default' do
+  to '/etc/apache2/sites-available/default'
+end
+
+template '/etc/thin/gnrd.yml' do
+  source 'thin_config.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
+
+template '/etc/init.d/thin' do
+  source 'thin_daemon.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
 package 'redis-server'
 
 package 'apache2'
@@ -31,20 +78,23 @@ package 'wget'
 gem_package 'mysql'
 
 
-script "gems install" do
+script "default ruby 1.9.3" do
   interpreter "bash"
   user "root"
-  cwd "/vagrant"
   code %Q{
     update-alternatives --set ruby /usr/bin/ruby1.9.3
     gem install bundle
-    gem install debugger
   }
+  not_if { "gem list | grep bundle" }
 end
 
 root_connection = { :host => 'localhost', 
-                :username => 'root', 
-                :password => node['mysql']['server_root_password'] }
+                    :username => 'root', 
+                    :password => node['mysql']['server_root_password'] }
+
+gnrd_connection = { :host => "localhost", 
+                    :username => node['mysql']['gnrd_user'], 
+                    :password => node['mysql']['gnrd_user_password']}
 
 mysql_database_user node['mysql']['gnrd_user'] do
   connection root_connection
@@ -60,16 +110,22 @@ mysql_database_user node['mysql']['gnrd_user'] do
 end
 
 mysql_database 'gnrd' do
-  connection ({:host => "localhost", :username => node['mysql']['gnrd_user'], :password => node['mysql']['gnrd_user_password']})
+  connection gnrd_connection
   action :create
 end
+
+# script 'global system upgrade' do
+#   interpreter 'bash'
+#   user 'root'
+#   code 'apt-get -q -y dist-upgrade'
+# end
 
 script "bundle install" do
   interpreter "bash"
   user "root"
   cwd "/vagrant"
   code %Q{
-    bundle install
+    bundle install --without development
   }
 end
 
@@ -78,7 +134,27 @@ script 'setup' do
   user 'root'
   cwd '/vagrant'
   code  <<-EOH
-    RACK_ENV=production rake migrate
-    RACK_ENV=production QUEUE=name_finder rake resque:work &
+    rake db:create:all
+    rake db:migrate
+    RACK_ENV=test rake db:migrate
+    RACK_ENV=production rake db:migrate
   EOH
 end
+
+service 'thin' do
+  action :restart
+end
+
+template '/etc/init/gnrd_worker.conf' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  source 'gnrd_worker.conf.erb'
+end
+
+service 'gnrd_worker' do
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :start => true, :restart => true
+  action [:enable, :restart]
+end
+
